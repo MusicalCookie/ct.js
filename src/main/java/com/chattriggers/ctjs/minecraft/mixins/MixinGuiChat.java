25ef8728a835f704389ce5cfdb63cf6c3e8d2f71
Hunter <thefalsehonesty@gmail.com>
2018-03-27 16:55:51 -0400

    Started on mixins (again)

---
+++ src/main/java/com/chattriggers/ctjs/minecraft/mixins/MixinGuiChat.java
@@ -0,0 +1,8 @@
+package com.chattriggers.ctjs.minecraft.mixins;
+
+import net.minecraft.client.gui.GuiChat;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GuiChat.class)
+public class MixinGuiChat {
+}
